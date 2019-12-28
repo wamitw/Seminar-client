954bd465cd177c1fa1808e922d95ff937f53d6e5
Amit Abecasis <wamitw@gmail.com>
2019-12-28 10:26:23 +0200

    client [New Feature]: Basic communication program

--- client.cpp
+++ client.cpp
@@ -5,10 +5,21 @@ using namespace std;
 
 int main (int argc, char **argv) {
 
+	int port;
+	string host;
 	if (argc < 2) {
 		cout << "missing arguments. run --help for help.\n";
 		return 1;
 	}
+
+	host = "127.0.0.1";
+	port = 8080;
+
+	MySocket s(host, port);
+	s.conn();
+	s.sendMsg(command);
+	ans = s.getMsg();
+	cout << "Answer Received!" << "\n" << ans;
 
 	return 0;
 }
