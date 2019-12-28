f35b7080d5863218c54688514840dcc5b067264d
Amit Abecasis <wamitw@gmail.com>
2019-12-28 10:23:18 +0200

    client [New Feature]: Define MySocket class

---
+++ MySocket.h
@@ -0,0 +1,27 @@
+/*
+ * socket.h
+ *
+ *  Created on: 11 Oct 2019
+ *      Author: amit
+ */
+
+#ifndef MYSOCKET_H_
+#define MYSOCKET_H_
+
+#include <arpa/inet.h>
+#include <string>
+
+class MySocket {
+private:
+	std::string host;
+	int fd, port;
+	struct sockaddr_in serv_addr;
+
+public:
+	MySocket(std::string, int);
+	int conn(void);
+	int sendMsg(std::string);
+	std::string getMsg(void);
+};
+
+#endif /* MYSOCKET_H_ */
