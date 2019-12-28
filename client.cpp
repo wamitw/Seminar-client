047cc0bb4b7af2a9a407282d08f30a0e2373a615
Amit Abecasis <wamitw@gmail.com>
2019-12-28 10:26:38 +0200

    client [New Feature]: Basic interpreter

--- client.cpp
+++ client.cpp
@@ -7,10 +7,23 @@ int main (int argc, char **argv) {
 
 	int port;
 	string host;
+	char c;
 	if (argc < 2) {
 		cout << "missing arguments. run --help for help.\n";
 		return 1;
 	}
+
+	command = "";
+	cout << ">>> ";
+	while(cin.get(c)) {
+		if(c == '\n')
+			cout << "\n" << ">>> ";
+		else
+			cout << c;
+		command += c;
+	}
+
+	cout << "\n\n" << "Command is:" << "\n" << command;
 
 	host = "127.0.0.1";
 	port = 8080;
