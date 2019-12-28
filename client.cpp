32e2740076f05cd8f40a852c08606369f42abb4d
Amit Abecasis <wamitw@gmail.com>
2019-12-28 10:26:04 +0200

    client [New Feature]: Check the number of arguments

--- client.cpp
+++ client.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include "MySocket.h"
+
+using namespace std;
+
 int main (int argc, char **argv) {
+
+	if (argc < 2) {
+		cout << "missing arguments. run --help for help.\n";
+		return 1;
+	}
 
 	return 0;
 }
